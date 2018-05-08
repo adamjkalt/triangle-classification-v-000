307967c220316b1d590ad7ce2164c2d1a643c450
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-08 04:17:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,16 +8,14 @@ def initialize(one, two, three)
   @three = three
 end
 
-# def validate_triangle
-#   real_triangle = [(one + two > three), (one + three > two), (two + three > one)]
-#   [one, two, three].each {|s| real_triangle << false if s <= 0}
-#   raise TriangleError if real_triangle.include?(false)
-# end
+def validate_triangle
+  real_triangle = [(one + two > three), (one + three > two), (two + three > one)]
+  [one, two, three].each {|s| real_triangle << false if s <= 0}
+  raise TriangleError if real_triangle.include?(false)
+end
 
 def kind
-  # validate_triangle
-  if self.class != Triangle
-        raise TriangleError
+  validate_triangle
   elsif one == two && two == three
     :equilateral
   elsif one == two || two == three || one == three
